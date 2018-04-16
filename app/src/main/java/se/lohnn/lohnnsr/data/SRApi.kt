db1ec93a37d376f8eeddf862e8cd522ea02ef06a
Johannes Löhnn <johannes.lohnn@infomaker.se>
2018-04-16 19:18:34 +0200

    Removed comment

--- app/src/main/java/se/lohnn/lohnnsr/data/SRApi.kt
+++ app/src/main/java/se/lohnn/lohnnsr/data/SRApi.kt
@@ -5,7 +5,6 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface SRApi {
-    //?format=json&size=40&page=1
     @GET("api/v2/programs?format=json&size=40")
     fun programs(@Query("page") page: Int = 1): Call<SRData>
 }
