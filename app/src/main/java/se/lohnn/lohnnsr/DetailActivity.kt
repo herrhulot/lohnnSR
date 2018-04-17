92ef069fd54036739d2c23a6510bf1ee54524891
Johannes Löhnn <johannes.lohnn@infomaker.se>
2018-04-17 20:02:13 +0200

    Content in DetailActivity

--- app/src/main/java/se/lohnn/lohnnsr/DetailActivity.kt
+++ app/src/main/java/se/lohnn/lohnnsr/DetailActivity.kt
@@ -2,14 +2,16 @@ package se.lohnn.lohnnsr
 
 import android.content.Context
 import android.content.Intent
-import android.support.v7.app.AppCompatActivity
+import android.databinding.DataBindingUtil
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import se.lohnn.lohnnsr.data.Program
+import se.lohnn.lohnnsr.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
 
     companion object {
-        private const  val PROGRAM_KEY: String = "programKey"
+        private const val PROGRAM_KEY: String = "programKey"
         fun createIntent(context: Context, program: Program): Intent =
                 Intent(context, DetailActivity::class.java).apply {
                     putExtra(PROGRAM_KEY, program)
@@ -18,6 +20,9 @@ class DetailActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+
+        DataBindingUtil.setContentView<ActivityDetailBinding>(this, R.layout.activity_detail).also { binding ->
+            binding.data = intent.getSerializableExtra(PROGRAM_KEY) as Program
+        }
     }
 }
