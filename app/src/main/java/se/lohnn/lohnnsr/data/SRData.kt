2203469cb1d63fa096c97c2e42c8579dc344146c
Johannes Löhnn <johannes.lohnn@infomaker.se>
2018-04-17 20:19:18 +0200

    Pimp my views

--- app/src/main/java/se/lohnn/lohnnsr/data/SRData.kt
+++ app/src/main/java/se/lohnn/lohnnsr/data/SRData.kt
@@ -4,4 +4,4 @@ import java.io.Serializable
 
 data class SRData(val copyright: String, val programs: List<Program>)
 
-data class Program(val name: String, val programimage: String, val description: String) : Serializable
+data class Program(val name: String, val description: String, val programimage: String, val programimagewide: String) : Serializable
